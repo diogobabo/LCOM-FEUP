69cd89aeb5b900f4e1e6377ca76b88408c7672e4
oliveira <jlp.penafiel@gmail.com>
2022-03-23 17:56:34 +0000

    refactored

--- lab2/timer.c
+++ lab2/timer.c
@@ -7,7 +7,7 @@
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
   
-  if(freq > TIMER_FREQ || freq<19){
+  if(freq > TIMER_FREQ || freq<19){ // frequencias invalidas
     return 1;
   }
 
@@ -18,25 +18,26 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
     return 1;
   }
 
-  st &= 0x0f;
-  st = st | TIMER_LSB_MSB;
+  st &= 0x0F;
+  st = st | TIMER_LSB_MSB; // resetar os bits todos menos os 4 lsbs
 
   if(timer == 0){
-    st = st|TIMER_SEL0|TIMER_LSB_MSB;
+    st = (st|TIMER_SEL0|TIMER_LSB_MSB);
   }
   else if(timer == 1){
-    st = st|TIMER_SEL1|TIMER_LSB_MSB;
+    st = (st|TIMER_SEL1|TIMER_LSB_MSB);
   }
   else if(timer == 2){
-    st = st|TIMER_SEL2|TIMER_LSB_MSB;
+    st = (st|TIMER_SEL2|TIMER_LSB_MSB);
   }
 
-  uint16_t timer_freq = TIMER_FREQ /freq;
+  uint16_t timer_freq = TIMER_FREQ / freq; // dividir a freq default pela rate HZ;
   
   uint8_t msb,lsb;
   util_get_LSB(timer_freq,&lsb);
   util_get_MSB(timer_freq,&msb);
 
+  // envia a control word para o reg controlo e dps mete a frequencia nos timers
   return sys_outb(TIMER_CTRL,st)||sys_outb(TIMER_0+timer,lsb)||sys_outb(TIMER_0+timer,msb);
 }
 
@@ -72,10 +73,9 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
     printf("SYS INB NOT WORKING");
     return 1;
   }
-  //printf("%s is not yet implemented!\n", __func__);
   return 0;
 }
-enum timer_init init_mode_func(uint8_t *st){
+enum timer_init init_mode_func(uint8_t *st){ // funcao feita para saber o mode de inicialização
 
   *st = *st >> 4; // por os bits 4 e 5 no inicio
   *st &= 0x03; // fazer & com 00000011, assim então dá os bits 4 e 5
@@ -90,10 +90,10 @@ enum timer_init init_mode_func(uint8_t *st){
     return MSB_after_LSB;
   }
   else{
-    return INVAL_val;
+    return INVAL_val; // caso seja invalido
   }
 }
-uint8_t count_mode_func(uint8_t st){
+uint8_t count_mode_func(uint8_t st){ // funcao feita para ajudar a saber o count mode,
 
   if(st == 0x06){
     return 2;
@@ -108,7 +108,6 @@ uint8_t count_mode_func(uint8_t st){
 }
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
   union timer_status_field_val test;
 
   if(field == tsf_all){
--- lab2/utils.c
+++ lab2/utils.c
@@ -3,22 +3,21 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  uint16_t temp = 0x00FF;
+  uint16_t temp = 0x00FF; // mete tudo a 0, menos o LSB
   temp = temp & val;
-  *lsb = (uint8_t) temp;
+  *lsb = (uint8_t) temp; // cast para 8 bits
   return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  uint16_t temp = 0xFF00;
+  uint16_t temp = 0xFF00; // mete tudo a 0, menos o MSB
   temp = temp & val;
-  temp = temp >> 8;
-  *msb = (uint8_t) temp;
+  temp = temp >> 8; // mete no sitio do LSB
+  *msb = (uint8_t) temp; // cast para 8 bits
   return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
   u32_t val = 0;
 
   int in;
