e3ccacd82c570f9dab173e79b6a163acf56b82ac
diogobabo <diogo.babo@gmail.com>
2022-06-11 23:33:13 +0100

    bug fix

--- proj/src/snake.c
+++ proj/src/snake.c
@@ -242,19 +242,16 @@ int CheckColisions(){
       return 0;
     }
     else if((snake.x == array[i].x * PIXELOFFSET) && (snake.y == array[i].y * PIXELOFFSET) && (array[i].active) && (array[i].type == BLOCK)) {
-      //GameState = EXIT;
       GameOverLeaderBoard();
       return 0;
     }
   }
   if(snake.x <= PIXELOFFSET - 1 || snake.x >= (23 * PIXELOFFSET) || snake.y <= PIXELOFFSET - 1 || snake.y >= ((17 * PIXELOFFSET) - 1)) {
-      //GameState = EXIT;
       GameOverLeaderBoard();
       return 0;
     }
   for(int i = 0; i < snake.bodySize; i++) {
     if((snake.x == snake.bodyX[i] * PIXELOFFSET) && (snake.y == snake.bodyY[i] * PIXELOFFSET)) {
-      //GameState = EXIT;
       GameOverLeaderBoard();
       return 0;
     }
